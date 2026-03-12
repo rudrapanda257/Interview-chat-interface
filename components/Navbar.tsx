2819f585cfe1e53872c3ff20767dc0c897115ec7
rudranarayanpanda05 <rudranarayanpanda05@gmail.com>
2026-03-12 13:09:21 +0530

    Update Navbar.tsx
    change sign In to Log In

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -129,7 +129,7 @@ export default function Navbar() {
                 </div>
               ) : (
                 <Button asChild className="bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 transform hover:scale-105 active:scale-95 transition-all duration-300">
-                  <Link href="/login">Sign In</Link>
+                  <Link href="/login">Log In</Link>
                 </Button>
               )}
             </div>
